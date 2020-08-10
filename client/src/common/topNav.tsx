9d15fd55925f7de1653fb11901335e287f17eac5
Hasanuzzaman <shuvo009@yahoo.com>
2020-08-11 01:06:36 +0600

    top nav bar

---
+++ client/src/common/topNav.tsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+export class TopNav extends Component {
+    render() {
+        return (
+            <Navbar>
+                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                <Nav className="justify-content-end ml-auto">
+                    <NavDropdown title="Profile" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="#action/3.1">My Profile</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">Edit Profile</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action/3.4">Settings</NavDropdown.Item>
+                    </NavDropdown>
+                    <Nav.Link href="#home">Inverview Invites</Nav.Link>
+                    <Nav.Link href="#features">Inbox</Nav.Link>
+                    <Nav.Link href="#pricing">Sign Out</Nav.Link>
+                </Nav>
+            </Navbar>
+        )
+    }
+}
--- client/src/layouts/privateLayout.tsx
+++ client/src/layouts/privateLayout.tsx
@@ -2,12 +2,16 @@ import React, { Component } from "react";
 import { Container } from 'react-bootstrap';
 import { LocalStorageHelper, Routes } from "../helpers";
 import { Redirect } from 'react-router-dom';
-
+import { TopNav } from "../common/topNav"
 export class PrivateLayout extends Component {
     render() {
         const { children } = this.props;
         if (LocalStorageHelper.isUserAuthorized) {
-            return (<Container>{children}</Container>)
+            return (
+                <Container>
+                    <TopNav></TopNav>
+                    {children}
+                </Container>)
         }
         return (<Redirect to={{ pathname: Routes.login }} />)
     }
